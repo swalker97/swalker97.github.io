e1e65cc8803171d8f1613d749754ecbf18290be7
Samantha Walker <swalker9@nd.edu>
2019-03-26 23:26:11 -0400

    project 2 deliverables

---
+++ Walker_project2/test-js/test-popup.js
@@ -0,0 +1,314 @@
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#task-reminder').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#task-reminder-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#submit-answer').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#submit-answer-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+
+
+// Button to open a modal
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#jordan').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#hint-2").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#hint-2').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#hint-2").toggleClass("visible");
+        $("#jordan").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-here').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-here").toggleClass("invisible");
+        $("#sun").toggleClass("visible");
+      
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-1').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-1").toggleClass("invisible");
+        $("#show-1").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-2').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-2").toggleClass("invisible");
+        $("#show-2").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-3').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-3").toggleClass("invisible");
+        $("#show-3").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-4').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-4").toggleClass("invisible");
+        $("#show-4").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-5').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-5").toggleClass("invisible");
+        $("#show-5").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-6').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-6").toggleClass("invisible");
+        $("#show-6").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-7').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-7").toggleClass("invisible");
+        $("#show-7").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-8').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-8").toggleClass("invisible");
+        $("#show-8").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-9').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-9").toggleClass("invisible");
+        $("#show-9").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-10').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-10").toggleClass("invisible");
+        $("#show-10").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-11').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-11").toggleClass("invisible");
+        $("#show-11").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#submit-close').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#submit-answer-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#task-reminder-close').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#task-reminder-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+
+//************************ Game Two ************************
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#apple-task-reminder').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#apple-task-reminder-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#apple-submit-answer').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#apple-submit-answer-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+
+
+// Button to open a modal
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#hint').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#hint-apple").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#hint-apple').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#hint-apple").toggleClass("visible");
+        $("#hint").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple1').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple1").toggleClass("invisible");
+        $("#show-apple1").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple2').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple2").toggleClass("invisible");
+        $("#show-apple2").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple3').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple3").toggleClass("invisible");
+        $("#show-apple3").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple4').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple4").toggleClass("invisible");
+        $("#show-apple4").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple5').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple5").toggleClass("invisible");
+        $("#show-apple5").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple6').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple6").toggleClass("invisible");
+        $("#show-apple6").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple7').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple7").toggleClass("invisible");
+        $("#show-apple7").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#click-apple8').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#click-apple8").toggleClass("invisible");
+        $("#show-apple8").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#apple-submit-close').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#apple-submit-answer-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#apple-task-reminder-close').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#apple-task-reminder-modal").toggleClass("visible");
+        $(".overlay").toggleClass("visible");
+        });
+});
+
+
+//************************ Templates ************************
+
+// Button to open a modal
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#place-button-name-here').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#place-modal-name-here").toggleClass("visible");
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#place-modal_close-icon-name-here').click(function() {
+        $("#place-modal-name-here").toggleClass("visible")
+        });
+});
